aa9528c18d35c2789fcda0590a7f270a691bc8f5
Eduardo Batista <91755689+Edu-B@users.noreply.github.com>
2024-05-02 17:53:36 -0400

    feat: add job domain (#9)

---
+++ src/alfred/rest/jobs/base.py
@@ -0,0 +1,24 @@
+# Native imports
+from typing import Text, Any
+from src.alfred.rest.jobs.typed import CreateJobDict
+from abc import ABC, abstractmethod
+
+
+class JobsBase(ABC):
+    @abstractmethod
+    def create(self, job: CreateJobDict) -> Any:
+        """
+        Creates a new Job.
+
+        Args:
+        - job: Job creation parameters.
+        """
+
+    @abstractmethod
+    def get(self, job_id: Text) -> Any:
+        """
+        Fetches a Job by its ID.
+
+        Args:
+        - job_id: Unique identifier of the Job.
+        """
